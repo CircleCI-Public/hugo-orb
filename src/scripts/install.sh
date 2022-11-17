2599f852e8652d4b8090a572b933ec6286993551
Jaryt Bustard <jarytbustard@gmail.com>
2022-11-17 15:18:02 -0500

    refactor: export html proofer (#55)

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -3,11 +3,11 @@ ORB_EVAL_INSTALL_LOCATION=$(eval echo "${ORB_EVAL_INSTALL_LOCATION}")
 
 case $OSD_FAMILY in
     linux)
-    OS=Linux
+    OS=Linux-64bit
     PKG_EXT=tar.gz
     ;;
     darwin)
-    OS=macOS
+    OS=darwin-universal
     PKG_EXT=tar.gz
     ;;
     *)
@@ -27,11 +27,12 @@ else
     HUGO_EXTENDED=""
 fi
 
-HUGO_URL=https://github.com/gohugoio/hugo/releases/download/v${ORB_VAL_VERSION}/hugo${HUGO_EXTENDED}_${ORB_VAL_VERSION}_${OS}-64bit.${PKG_EXT}
+HUGO_URL=https://github.com/gohugoio/hugo/releases/download/v${ORB_VAL_VERSION}/hugo${HUGO_EXTENDED}_${ORB_VAL_VERSION}_${OS}.${PKG_EXT}
 curl --fail -sSL "$HUGO_URL" -o hugo-archive 2>/dev/null
 # If the download fails...
-if grep < hugo-archive | $SUDO tar -xz -C "${ORB_EVAL_INSTALL_LOCATION}" hugo 2>/dev/null; then
-    echo "We're good, continue."
+
+if $SUDO tar -xzf hugo-archive -C "${ORB_EVAL_INSTALL_LOCATION}" hugo 2>/dev/null; then
+    echo "Hugo succesfully installed."
 else
     if [[ ! "${ORB_VAL_VERSION}" =~ ^[0-9]+\.[0-9]+\.[0-9]+ ]]; then
     echo "Failed to install. The version number ${ORB_VAL_VERSION} is not a full valid SemVer version."
